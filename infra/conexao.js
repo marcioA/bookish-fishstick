290ba8c7fb6771710e4e16dbdc9ccda0a32ec25f
marcioA <zamar_pinheiro@hotmail.com>
2023-03-10 19:52:13 -0300

    atualização com arquivos do projeto nodejs

---
+++ infra/conexao.js
@@ -0,0 +1,13 @@
+import mysql from 'mysql';
+
+// require('dotenv').config();
+
+const conexao = mysql.createConnection({
+    host: "localhost",
+    port: "3306",
+    user: "root",
+    password: "",
+    database: "",
+});
+
+export default conexao;
---
+++ routes.js
@@ -0,0 +1,55 @@
+import express from 'express';
+
+const routes = express.Router();
+
+routes.all('*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
+routes.get('/', (req, res) => {
+    return res.status(201).json({ hello: 'pagina inicial logo será implementada' })
+});
+
+routes.get('/transacao', (req, res) => {
+    return res.status(201).json({
+        msg: "Retornou o get type module"
+    })
+});
+
+routes.get('/transacao/:id', (req, res) => {
+    const { id } = req.params;
+    return res.status(200).json({
+        id,
+        msg: "Ok"
+    })
+});
+
+routes.post('/transacao', (req, res) => {
+    const { titulo, valor, tipo, categoria } = req.body;
+    var transacao = {
+        id: 1,
+        "title": titulo,
+        "transaction_value": valor,
+        "transaction_type": tipo,
+        "product_owner": categoria,
+    }
+
+    return res.status(200).json(transacao)
+})
+
+routes.put('/transacao/:id', (req, res) => {
+    const { id } = req.params;
+    return res.status(200).json({
+        msg: "Ok"
+    })
+})
+
+
+routes.delete('/transacao/:id', (req, res) => {
+    const { id } = req.params;
+    return res.status(200).json({ msg: 'o registro "' + id + '" foi apagado com sucesso.' })
+});
+
+export default routes;
